87a9dcbd576920432368dddc7e16d63505395650
Nainai777 <natalianogueir2006@gmail.com>
2025-04-14 15:28:59 -0300

    Listas de exercicios: Lista 04: exercicio_02

--- Listas de Exercícios/LP_Lista_04/exercicio_02/src/Main.java
+++ Listas de Exercícios/LP_Lista_04/exercicio_02/src/Main.java
@@ -5,14 +5,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int n;
-        Scanner in = new Scanner(System.in);
-
-        System.out.println("Digite um número:");
-        n = in.nextInt();
-
-        for (int i = 0; i < 11; i++){
-            System.out.println(n+" x "+i+" = "+ (i*n));
+        Scanner scanner = new Scanner(System.in);
+        // Solicitação do primeiro número
+        System.out.print("Digite o 1º número: ");
+        int numero = scanner.nextInt();
+        // Inicializa maior e menor com o primeiro número lido
+        int maior = numero;
+        int menor = numero;
+        // Leitura e comparação dos próximos números
+        for (int i = 2; i <= 5; ++i) {
+            System.out.print("Digite o " + i + "º número: ");
+            numero = scanner.nextInt();
+            // Atualização do maior e menor, se necessário
+            if (numero > maior) {
+                maior = numero;
+            } else if (numero < menor) {
+                menor = numero;
+            }
         }
+        // Exibição do resultado
+        System.out.println("O maior número é: " + maior);
+        System.out.println("O menor número é: " + menor);
+        // Fechar o scanner para evitar vazamentos de recursos
+        scanner.close();
     }
 }
